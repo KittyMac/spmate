96c2308d2741d6171ed8c610d95a9dfa3758cd88
Rocco Bowling <rocco@chimerasw.com>
2024-10-10 14:26:18 -0400

    WIP: build and install xcframework

---
+++ spmate/spmate/spmate.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+#ifndef _SPMATE_H_
+#define _SPMATE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+void spmate_test();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
